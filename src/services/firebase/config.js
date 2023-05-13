8e015e89639a533ff2166fc80fae1bb97b692820
Mati Ezek <matiezequielcamba@gmail.com>
2023-05-13 01:28:04 -0300

    modifique las keys

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -5,12 +5,12 @@ import {getFirestore} from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVTNa1q1MYUfZb_XudkWfMDIbwfuJQOvw",
-  authDomain: "tienda-sofi.firebaseapp.com",
-  projectId: "tienda-sofi",
-  storageBucket: "tienda-sofi.appspot.com",
-  messagingSenderId: "1031466295280",
-  appId: "1:1031466295280:web:6c0013ddf6e5944723c735"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 
